18f3360e05ca9eeadeaa55af7e436f0bf6c2a83a
Nicholas Roberts <niccolox@devekko.com>
2025-02-08 19:47:36 -0800

    product domain and resource

--- store/lib/store/product.ex
+++ store/lib/store/product.ex
@@ -1,12 +1,13 @@
 defmodule Store.Product do
-  use Ash.Domain, otp_app: :store, extensions: [AshGraphql.Domain, AshJsonApi.Domain]
+  use Ash.Domain, otp_app: :store, extensions: [AshGraphql.Domain, AshJsonApi.Domain, AshPhoenix]
 
   resources do
-    resource(Store.Product.Product)
-    define(:create_product, action: :create)
-    define(:update_product, action: :update)
-    define(:destroy_product, action: :destroy)
-    define(:read_product, action: :read)
-    define(:get_product_by_id, action: :read, get_by: :id)
+    resource(Store.Product.Product) do
+      define(:create_product, action: :create)
+      define(:update_product, action: :update)
+      define(:destroy_product, action: :destroy)
+      define(:read_product, action: :read)
+      define(:get_product_by_id, action: :read, get_by: :id)
+    end
   end
 end
--- store/lib/store/product/product.ex
+++ store/lib/store/product/product.ex
@@ -2,7 +2,7 @@ defmodule Store.Product.Product do
   use Ash.Resource,
     otp_app: :store,
     domain: Store.Product,
-    extensions: [AshGraphql.Resource, AshJsonApi.Resource, AshAdmin.Resource],
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource],
     data_layer: AshPostgres.DataLayer
 
   graphql do
@@ -20,17 +20,59 @@ defmodule Store.Product.Product do
 
   attributes do
     uuid_primary_key(:id)
-    attribute :sku, :string do allow_nil?(false) public?(true) end
-    attribute :name, :string do allow_nil?(false) public?(true) end
-    attribute :slug, :string do allow_nil?(false) public?(true) end
-    attribute :subtitle, :string do public?(true) end
-    attribute :description, :string do allow_nil?(false) public?(true) end
-    attribute :featured_image, :string do allow_nil?(false) public?(true) end
-    attribute :images, :map do public?(true) end
-    attribute :featured, :boolean do allow_nil?(false) public?(true) end
-    attribute :order, :integer do public?(true) end
-    attribute :stripe_id, :string do allow_nil?(false) public?(true) end
-    attribute :price, :decimal do allow_nil?(false) public?(true) end
+
+    attribute :sku, :string do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :name, :string do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :slug, :string do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :subtitle, :string do
+      public?(true)
+    end
+
+    attribute :description, :string do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :featured_image, :string do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :images, :map do
+      public?(true)
+    end
+
+    attribute :featured, :boolean do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :order, :integer do
+      public?(true)
+    end
+
+    attribute :stripe_id, :string do
+      allow_nil?(false)
+      public?(true)
+    end
+
+    attribute :price, :decimal do
+      allow_nil?(false)
+      public?(true)
+    end
+
     timestamps()
   end
 
